3c250d3b954e9c6c234fcf1acd6fa45eafa4838b
Hem Shrestha <hereshem@gmail.com>
2018-06-21 00:32:07 +0545

    updated library version to 2.1.1

--- app/src/main/java/com/hereshem/recyclerviewwithloadmoreandserverrequest/MainActivity.java
+++ app/src/main/java/com/hereshem/recyclerviewwithloadmoreandserverrequest/MainActivity.java
@@ -2,20 +2,19 @@ package com.hereshem.recyclerviewwithloadmoreandserverrequest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.hereshem.lib.recycler.MyRecyclerView;
+import com.hereshem.lib.recycler.MyViewHolder;
 import com.hereshem.lib.recycler.RecyclerViewAdapter;
+import com.hereshem.lib.server.MapPair;
+import com.hereshem.lib.server.Method;
 import com.hereshem.lib.server.MyDataQuery;
 import com.hereshem.lib.utils.Preferences;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static class VHolder extends RecyclerView.ViewHolder {
+    public static class VHolder extends MyViewHolder<Events> {
         TextView date, title, summary;
         public VHolder(View v) {
             super(v);
@@ -54,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             title = v.findViewById(R.id.title);
             summary = v.findViewById(R.id.summary);
         }
+        @Override
         public void bindView(Events c){
             date.setText(c.date);
             title.setText(c.title);
@@ -66,13 +66,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter<Events, VHolder>(this, items, VHolder.class, R.layout.row_contact) {
-            @Override
-            public void onBinded(VHolder holder, int position) {
-                holder.bindView(items.get(position));
-            }
-        };
-
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, VHolder.class, R.layout.row_contact);
         recycler = findViewById(R.id.recycler);
         recycler.setAdapter(adapter);
         recycler.setOnItemClickListener(new MyRecyclerView.OnItemClickListener() {
@@ -91,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadData() {
-        HashMap<String, String> maps = new HashMap<>();
-        maps.put("action", "get_day");
-        maps.put("start", start+"");
+        MapPair maps = new MapPair()
+                .add("action","get_events")
+                .add("start", start+"");
         new MyDataQuery(this, maps) {
             @Override
             public void onSuccess(String table_name, String result) {
@@ -111,22 +105,22 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public String onDbQuery(String table, HashMap<String, String> params) {
+            public String onDataQuery(String table) {
                 if(table.equals("0")){
                     return new Preferences(getApplicationContext()).getPreferences("data_downloaded");
                 }
-                return super.onDbQuery(table, params);
+                return super.onDataQuery(table);
             }
 
             @Override
-            public void onDbSave(String table, String response) {
+            public void onDataSave(String table, String response) {
                 if(table.equals("0")){
                     new Preferences(getApplicationContext()).setPreferences("data_downloaded", response);
                 }
             }
         }
         .setUrl("http://dl.mantraideas.com/apis/events.json")
-        .setMethod(MyDataQuery.Method.GET)
+        .setMethod(Method.GET)
         .setIdentifier(start+"")
         .execute();
     }
