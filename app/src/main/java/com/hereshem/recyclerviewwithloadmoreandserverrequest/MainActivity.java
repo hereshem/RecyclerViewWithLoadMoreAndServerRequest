57c4ea338f5e7b5df31cf81ad1d3bcaa08fef46d
Hem Shrestha <hereshem@gmail.com>
2018-08-07 23:06:46 +0545

    typos

--- app/src/main/java/com/hereshem/recyclerviewwithloadmoreandserverrequest/MainActivity.java
+++ app/src/main/java/com/hereshem/recyclerviewwithloadmoreandserverrequest/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
